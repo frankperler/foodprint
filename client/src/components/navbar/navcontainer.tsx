95742711ea8776208a1cb0c2540e8da128daf60a
Frank Perler <fperler@gmail.com>
2021-08-14 16:01:59 +0200

    feature: added navbar

---
+++ client/src/components/navbar/navcontainer.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Navcontainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #A5FFd6;
+  width: 100vw;
+  height: 5rem;
+`
---
+++ client/src/components/navbar/title.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  width: 3rem;
+  height: 2rem;
+  margin-left: 1rem;
+  color: #394141;
+  font-weight: normal;
+`
