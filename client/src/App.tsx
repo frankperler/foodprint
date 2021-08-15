beec671b3dd060fa885fa1a03e66cf9cb03ffb8a
aaronzomback <aaronzomback@gmail.com>
2021-08-15 14:40:01 +0200

    updated FormFoodLover inputs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FormRegister } from './components/registration/FormRegister'
+import { FormFoodLover } from './components/registration/FormFoodLover'
 import ChooseUserType from './components/registration/ChooseUserType'
 import { Searchbar } from './components/add-partner/Searchbar'
 import { FormLogIn } from './components/log-in/FormLogIn'
@@ -9,7 +9,7 @@ import { FormRestaurant } from './components/registration/FormRestaurant'
 function App(): JSX.Element {
   return <div className="App">
     <ChooseUserType></ChooseUserType>
-    <FormRegister></FormRegister>
+    <FormFoodLover></FormFoodLover>
     </div>
 }
 
