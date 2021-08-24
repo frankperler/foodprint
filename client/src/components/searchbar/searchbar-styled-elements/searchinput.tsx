c177e9b4863af99abec80383c60e4c55f618ec2a
Frank Perler <fperler@gmail.com>
2021-08-24 23:27:30 +0200

    style: decrease shadow of searchbar

--- client/src/components/searchbar/searchbar-styled-elements/searchinput.tsx
+++ client/src/components/searchbar/searchbar-styled-elements/searchinput.tsx
@@ -6,7 +6,7 @@ export const SearchInput = styled.input`
   height: 3rem;
   padding-left: 2em;
   border-radius: 25px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -2px, rgba(0, 0, 0, 0.3) 0px 2px 5px -3px;
   border: solid 0.1rem white;
   background-color: white;
   text-indent: 1rem;
