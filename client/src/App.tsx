e32342f14a966a98e673679e66f569c51e6397c5
Giulia Consonni <kawngc@gmail.com>
2021-08-24 16:04:16 +0200

    feat: userState and isAuth persist on refresh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import { ProfileRestaurantDashboard } from './components/profile/restaurant/prof
 import { userTypes } from './types'
 import { userContext } from './contexts/user-context'
 import { userLoginReducers, userLoginState } from './reducers/login-reducer';
+import { useEffect } from 'react'
 
 export const App: React.FunctionComponent = () => {
 
@@ -16,6 +17,17 @@ export const App: React.FunctionComponent = () => {
   const [userType, setUserType] = useState("Food lover")
   const [isAuth, setIsAuth] = useState(false)
   const [loading, setLoading] = useState(true);
+
+  //to persist state on refresh
+  useEffect(() => {
+    dispatchUser({ type: 'LOGIN', payload: JSON.parse(window.localStorage.getItem('user-state')!)});
+    setIsAuth(JSON.parse(window.localStorage.getItem('is-auth')!))
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem('user-state', JSON.stringify(stateUser))
+    window.localStorage.setItem('is-auth', JSON.stringify(isAuth))
+  })
 
   return (
     <userContext.Provider value={{ stateUser, dispatchUser }} >
