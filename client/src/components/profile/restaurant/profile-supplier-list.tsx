a73232bd50a5732f4dded7ac4d13830ba6e74aa9
Giulia Consonni <kawngc@gmail.com>
2021-08-25 20:03:29 +0200

    merge accepted

--- client/src/components/profile/restaurant/profile-supplier-list.tsx
+++ client/src/components/profile/restaurant/profile-supplier-list.tsx
@@ -14,18 +14,6 @@ export const SuppliersList: React.FunctionComponent<Props> = ({ restaurant, isOw
 
   return (
     <SupplierContainer>
-<<<<<<< HEAD
-      {!isOwner?  <div></div> :
-        <ButtonWrap>
-          <Link to="/add" style={{ textDecoration: 'none' }}>
-            <AddSupplier>
-              <AddCircle fontSize="large" />
-              <h4>Add supplier</h4>
-            </AddSupplier>
-          </Link>
-        </ButtonWrap>
-      }
-=======
       <ButtonWrap>
         <Link to="/add" style={{ textDecoration: 'none' }}>
           <AddSupplier style={{color: "#84DCC6"}}>
@@ -34,7 +22,6 @@ export const SuppliersList: React.FunctionComponent<Props> = ({ restaurant, isOw
           </AddSupplier>
         </Link>
       </ButtonWrap>
->>>>>>> 2051897e4a94cc79049d5a5dbb7d75c3f47b9a71
       <ProfileHeader>Who we get our food from...</ProfileHeader>
       <SupplierListContainer>
         {restaurant.Suppliers && restaurant.Suppliers.map((supplier: supplierTypes) => {
